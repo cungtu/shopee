a7e108b6b8810ef4d1d9c34373d909350d001cb8
vds-tcung-tibco <tcung@tibco.com>
2024-04-29 17:59:25 +0700

    h2

--- src/main/java/com/revise/shopee/configuration/SecurityConfiguration.java
+++ src/main/java/com/revise/shopee/configuration/SecurityConfiguration.java
@@ -39,9 +39,6 @@ public class SecurityConfiguration   {
                 .build();
         return new InMemoryUserDetailsManager(userDetails);
     }
-
-
-
 }
 
 
--- src/main/java/com/revise/shopee/repository/UserRepository.java
+++ src/main/java/com/revise/shopee/repository/UserRepository.java
@@ -1,11 +1,11 @@
-//package com.revise.shopee.repository;
-//
-//import com.revise.shopee.model.User;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//import org.springframework.stereotype.Service;
-//
-//@Repository
-//public interface UserRepository extends JpaRepository<User, Long> {
-//}
+package com.revise.shopee.repository;
+
+import com.revise.shopee.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
